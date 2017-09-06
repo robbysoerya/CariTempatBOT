e4aa3d621e46928f01dc5e676da090d6b6c7a7fe
fahtul <fahtulabdulrahman@gmail.com>
2017-09-06 22:10:50 +0700

    implementasi reply message

--- index.php
+++ index.php
@@ -66,11 +66,14 @@ if(is_array($data['events'])){
             {
 
 
+         
                 // send same message as reply to user
-                $result = $bot->replyText($event['replyToken'], $event['message']['location'][0]);
+         
+				$locationMessageBuilder = new LocationMessageBuilder($event['message']['location']);
+                $result = $bot->replyMessage($event['replyToken'], $locationMessageBuilder);
 
                 // or we can use replyMessage() instead to send reply message
-                // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
+         
                 // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
 
                 return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
